e647f15770ac876fd511ae8ec449ce409b5d5f5e
RakitDautov <rocke1995@gmail.com>
2021-05-23 17:34:51 +0300

    test 1

--- api/permissions.py
+++ api/permissions.py
@@ -4,13 +4,13 @@ from rest_framework import permissions
 class IsAuthorOrAdminOrModeratorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
-                obj.author == request.user or
-                request.method in permissions.SAFE_METHODS or
-                request.user.is_admin or request.user.is_moderator
+            obj.author == request.user or
+            request.method in permissions.SAFE_METHODS or
+            request.user.is_admin or request.user.is_moderator
         )
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.method in permissions.SAFE_METHODS or \
-               (request.user and request.user.is_staff)
+           (request.user and request.user.is_staff)
